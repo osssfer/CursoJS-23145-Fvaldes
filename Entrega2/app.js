c7d0e996ec4531aa24c31a6b53654ba60e06b46a
FernandoJR <osfer93@gmail.com>
2021-08-04 22:21:25 -0400

    Adding Entrega 2

--- Entrega2/app.js
+++ Entrega2/app.js
@@ -1,18 +1,29 @@
 //Nombre Usuario
-const userName = prompt(' ingrese nombre ')
+const userName = prompt(' ingrese nombre ').toUpperCase()
+
+// Input año de nacimiento
 let userYear = prompt(' Ingrese año de nacimiento ')
 
+// Comienzo de condicionales segun fecha año de nacimiento
 
-if (userYear > 2021){
-    alert(' No te hagas el pillo aun no haz nacido '+ userName)
+if ( Object.is(userYear)  === false  ){
+    alert(  " Bua .. ese no es un numero.. " + userName )
+    alert(' Intenantalo otra vez... Apreta F5 ' + userName)
+
+}else if(userYear > 2021){
+    alert( ' No te hagas el pillo aun no haz nacido ' + userName )
+    
     
 }else if(2020 > userYear && userYear > 1990 ){
-    alert(' Eres GENERACION Z '+ userName)
+    alert( ' GENERACION Z ' + userName )
     
 }else if (1989 > userYear && userYear > 1960 ){
-    alert(' ERES  MILLENIEAL ! ' + userName)
+    alert('   MILLENIEAL  ' + userName)
+    
+}else if (1959 > userYear && userYear > 0 ){
+    alert(' BOOMER  ' + userName)
     
 }else{
-    alert(' BOOMER '  + userName)
+    alert(' DUDO QUE ESTES VIVO    '  + userName)
  
 }
